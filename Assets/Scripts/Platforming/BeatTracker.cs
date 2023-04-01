56115cdf684ee912fc92136536e101bf245178cd
Unknown <ncarbonara235@gmail.com>
2023-04-01 17:43:38 -0400

    Started Cleaning Codebase for New Team
    Removed superfluous files from old tests/features, renamed scenes/variables for clarity, added new folders, added more code comments, etc.

--- Assets/Scripts/Platforming/BeatTracker.cs
+++ Assets/Scripts/Platforming/BeatTracker.cs
@@ -4,37 +4,44 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
+/// <summary>
+/// The script responsible for determining what the song's tempo is and using that information to control various aspects of the game.
+/// </summary>
 public class BeatTracker : MonoBehaviour
 {
-    public static BeatTracker instance;
+    public static BeatTracker instance; //Singleton
 
-    public GameObject welcomeMessage;
-    public AnimationCurve linearCurve;
-    private GameObject playerCanvas;
+    public GameObject welcomeMessage;   //Text message that greets the player before they start the level
+    private GameObject playerCanvas;    //Canvas parented to the player, used to display text UI that should be attached to the player
     private GameObject screenSpaceCanvas;
+
+    public AnimationCurve linearCurve;  //Used for lerp calculations
+
     public AudioSource songPlayer;
-    public bool startedLevel;
 
-    public float timeTracker;
-    float nextBeatLocation; //The "location" of the next beat on the timeline. Periodically pushed ahead as the music continues in order to reflect when the next beat will be.
-    private float twoBeatsLength;   //The length in seconds between two beats in the song's tempo (the pace at which the drums in "wishing well" -- the ones that the player needs to match -- hit)
-    public float forgivenessRange;  //The amount of time in seconds that the player can be "early" or "late" in hitting the beat, but still have it count as a successful dash
-    public bool onBeat;
-    public float bpm;
-    bool playerDashedThisBeat;  //States whether/not the player successfully dashed on the previous beat
+    public float timeTracker;   //How much time has passed since the player started the level; incremented with each frame
+    float startTime;    //The time at which the player started the level
 
-    public TMP_Text clock;
-    public float clockTime;
+    public float clockTime; //Used to track how much time left in the song the player has to complete the level
     public Image clockBar;
-    public bool canClick;   //Determines if the player has "spent" their attempt to click/dash for the beat they're on
-    float startTime;
 
-    private float rhythmIndicatorTimer;
+    public bool startedLevelCountdown;   //Changes based on whether/not the player has started the level
+
+    float nextBeatTime; //The "location" in time of the next beat. Periodically pushed ahead as the music continues to reflect when the next beat will be.
+    private float twoBeatsLength;   //The length in seconds between two beats in the song's tempo. (Important b/c the rhythm that matters for the dash mechanic occurs only once every *two* beats of the song's tempo)
+    public float forgivenessRange;  //The amount of time in seconds that the player can be "early" or "late" in hitting the beat, but still have it count as a successful dash
+    public bool onBeat; //States whether or not the player has accurately clicked/tapped to the most recent beat
+    public float bpm;   //"Beats per minute" of the song
+    bool playerDashedThisBeat;  //States whether/not the player successfully dashed on the previous beat
+    public bool canClick;   //Determines if the player has "spent" their attempt to click/dash for the beat they're on
+
+    //Variables associated with the rhythm indicator (the pair of bars that appears above the player's head)
+    private float rhythmIndicatorTimer; //Used to track when the rhythm indicator should spawn a new set of "metronome bars"
     public static bool needNewRhythmIndicatorBars1;
     public static bool needNewRhythmIndicatorBars2;
     public GameObject bar;
     public bool barDebugMode;  //A debug tool to help the developer test the rhythm "forgiveness" value and see whether a pair of bars will count as a hit or not before clicking
-    private Color barDebugColor;
+    private Color barColor;
     GameObject newBarL;
     GameObject newBarR;
 
@@ -60,7 +67,7 @@ public class BeatTracker : MonoBehaviour
         songPlayer.volume = .5f;
 
         twoBeatsLength = 60f / bpm;
-        nextBeatLocation = twoBeatsLength + (twoBeatsLength / 4f);    //Added to have the "beat" land on the second and fourth beats of each measure
+        nextBeatTime = twoBeatsLength + (twoBeatsLength / 4f);    //Added to have the "beat" land on the second and fourth beats of each measure
         rhythmIndicatorTimer -= ((8f * twoBeatsLength)); //Offsets rhythmIndicatorTimer so that the bars don't start appearing until the percussion beats of "wishing well" begin, roughly four measures in
 
         clockTime = songPlayer.clip.length;
@@ -83,43 +90,46 @@ public class BeatTracker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(startedLevel)
+        //When the player has clicked/tapped to begin the level
+        if(startedLevelCountdown)
         {
             TriggerTextMessages();
 
-            //Resets the level if the player presses the "R" key. (This can also be done by clicking/tapping a button on the game screen)
+            //Resets the level if the player presses the "R" key
             if(Input.GetKeyDown(KeyCode.R))
             {
                 ResetLevel();
             }
 
+            timeTracker += Time.deltaTime;  //Updates the script's understanding of how much time has passed
+
+            //Sets the amount of the timer bar UI that's still filled in
             clockTime = songPlayer.clip.length - (Time.time - startTime);
-            //Debug.Log(clockTime);
-            clock.text = "" + clockTime;
             clockBar.fillAmount = clockTime / songPlayer.clip.length;
-            timeTracker += Time.deltaTime;
 
             //Checks if/when a new pair of "metronome bars" should appear
             UpdateRhythmIndicator();
 
+            //Checks to see if the countdown is over
             if(countdownFinished)
             {
                 //Checks if the player is clicking/tapping on the beat
-                if(timeTracker > (nextBeatLocation - forgivenessRange) && timeTracker < (nextBeatLocation + forgivenessRange))
+                if(timeTracker > (nextBeatTime - forgivenessRange) && timeTracker < (nextBeatTime + forgivenessRange))
                 {
                     if(Input.GetMouseButtonDown(0))
                     {
                         Debug.Log("Hit");
 
                             //Disabling combo functionality for now
+
                             //GameManager.instance.dashCombos++;  //Increments the player's combo number
                             //playerDashedThisBeat = true;    //Saved to look at next beat and determine if the player's combo value should be reset
                     }
 
-                    //Sets what color the bar will be if the player clicks on this frame
-                    barDebugColor = Color.yellow;
+                    barColor = Color.yellow;   //Sets what color the bar will be if the player clicks on this frame
 
                     onBeat = true;
+
                 } else
                 {
                     if(Input.GetMouseButtonDown(0))
@@ -130,28 +140,28 @@ public class BeatTracker : MonoBehaviour
                         //GameManager.instance.dashCombos = 0;    //Resets the player's combo number
                     }
 
-                    barDebugColor = Color.red;
+                    barColor = Color.red;
 
                     onBeat = false;
                 }
 
                 //Updates nextBeatLocation each time the current beat is passed
-                if(timeTracker > nextBeatLocation + forgivenessRange)
+                if(timeTracker > nextBeatTime + forgivenessRange)
                 {
-                    nextBeatLocation += twoBeatsLength;
+                    nextBeatTime += twoBeatsLength;
                     canClick = true;    //Refreshes the player's attempt to click/dash
                 }
             }
 
         } else
         {
-            //Start Song
+            //Start music/level
             if (Input.GetMouseButtonDown(0))
             {
                 welcomeMessage.SetActive(false);
                 songPlayer.Play();
                 startTime = Time.time;
-                startedLevel = true;
+                startedLevelCountdown = true;
             }
         }
         
@@ -301,12 +311,12 @@ public class BeatTracker : MonoBehaviour
             //Changes bar color _before_ player clicks, but only if debug mode is on
             if(barDebugMode == true)
             {
-                newBar.GetComponent<Image>().color = barDebugColor;
+                newBar.GetComponent<Image>().color = barColor;
             }
 
             if(Input.GetMouseButtonDown(0))
             {
-                newBar.GetComponent<Image>().color = barDebugColor;    //Changes the bar's color after the player has clicked, so they can see whether/not they hit it
+                newBar.GetComponent<Image>().color = barColor;    //Changes the bar's color after the player has clicked, so they can see whether/not they hit it
                 t = 1;
 
                 instaDestroyBar = false;
@@ -361,12 +371,12 @@ public class BeatTracker : MonoBehaviour
             //Changes bar color _before_ player clicks, but only if debug mode is on
             if(barDebugMode == true)
             {
-                bar.GetComponent<Image>().color = barDebugColor;
+                bar.GetComponent<Image>().color = barColor;
             }
 
             if(Input.GetMouseButtonDown(0))
             {
-                bar.GetComponent<Image>().color = barDebugColor;    //Changes the bar's color after the player has clicked, so they can see whether/not they hit it
+                bar.GetComponent<Image>().color = barColor;    //Changes the bar's color after the player has clicked, so they can see whether/not they hit it
                 t = 1;
 
                 instaDestroyBar = false;
