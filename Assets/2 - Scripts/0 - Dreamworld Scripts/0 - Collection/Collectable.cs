30e2e08cf7a87bac087a259071986525241a6d85
seamusly <seamus.ly@outlook.com>
2023-10-30 02:01:46 -0400

    Randomize thought fragment message

--- Assets/2 - Scripts/0 - Dreamworld Scripts/0 - Collection/Collectable.cs
+++ Assets/2 - Scripts/0 - Dreamworld Scripts/0 - Collection/Collectable.cs
@@ -1,4 +1,3 @@
-using System;
 using RoboRyanTron.Unite2017.Events;
 using TMPro;
 using UnityEngine;
@@ -15,10 +14,61 @@ public class Collectable : MonoBehaviour, ICollectable
     private Vector3 startingLocation;
     [SerializeField] private GameObject display;
     [SerializeField] private Collider2D collider;
+
+    [SerializeField] private string currentMessage;
+    private string[] activeMessages;
+
+    #region Thought Fragment Messages
+    private string[] genericMessages = new string[7]
+    {
+        "These nights are too long",
+        "Need to take my time",
+        "Do they care about me?",
+        "I’m self-conscious",
+        "Is anyone there?",
+        "I got this... or maybe not",
+        "Can’t fake it, can’t make it"
+
+    };
+
+    private string[] level1Messages = new string[14]
+    {
+        "Tired of conversations",
+        "Just have to love myself",
+        "Been waiting for a long time",
+        "Mystified I’m still walking",
+        "Still wandering",
+        "Don’t need nobody else",
+        "I might cry",
+        "Spend my time within a dream",
+        "Bring me life",
+        "What am I waiting for?",
+        "Hoping someone would be so kind",
+        "Dime in the wishing well",
+        "I’m waiting for you",
+        "Stuck in my mind"
+    };
+    #endregion
+
     private void Start()
     {
         startingLocation = transform.position;
         OnCollect.Register?.Invoke();
+
+        activeMessages = new string[genericMessages.Length + level1Messages.Length];
+
+        for (int i = 0; i < activeMessages.Length; i++)
+        {
+            if (i < genericMessages.Length)
+            {
+                activeMessages[i] = genericMessages[i];
+            } else
+            {
+                activeMessages[i] = level1Messages[i - genericMessages.Length];
+            }
+        }
+
+        InvokeRepeating(nameof(RandomizeMessage), 0f, 5f);
     }
 
     public void Collect()
@@ -41,5 +91,12 @@ public class Collectable : MonoBehaviour, ICollectable
         {
             Collect();
         }
+    }
+
+    private void RandomizeMessage()
+    {
+        int randomIndex = Random.Range(0, activeMessages.Length - 1);
+
+        currentMessage = activeMessages[randomIndex];
     }
 }
