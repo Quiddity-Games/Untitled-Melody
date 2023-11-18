c3eae86b561f74b002b6c31a4324f09abde340e4
seamusly <seamus.ly@outlook.com>
2023-11-18 18:41:16 -0500

    Update to contrast layer values and systems

--- Assets/2 - Scripts/0 - Dreamworld Scripts/ContrastLayer.cs
+++ Assets/2 - Scripts/0 - Dreamworld Scripts/ContrastLayer.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 
 public class ContrastLayer : MonoBehaviour
 {
-    [SerializeField] private BoolVariable contrastLayerEnabled;
-    [SerializeField] private BoolVariable isContrastLayerBlack;
-    [SerializeField] private FloatVariable opacity;
+    [SerializeField] private BoolVariable _contrastLayerEnabled;
+    [SerializeField] private Color[] _contrastLayerColors;
+    private int _savedColor;
+    private int _savedOpacity;
     private SpriteRenderer layerRenderer;
 
     // Start is called before the first frame update
@@ -15,23 +16,44 @@ public class ContrastLayer : MonoBehaviour
     {
         layerRenderer = GetComponent<SpriteRenderer>();
 
-        if (isContrastLayerBlack.Value)
-            layerRenderer.color = Color.black;
-        else
-            layerRenderer.color = Color.white;
+        // For Justin + Save System: save out the index of the colours and set loadedColor to grab it on start.
+        // For now, the default will always be 0/black.
+        // Same thing with the opacity.Right now, it's taking a value between 0-100 and dividing it by 100 to get the alpha.
+        // Feel free to change it to a number that's easier to save out--I figured whole numbers worked best!
 
-        if (contrastLayerEnabled.Value)
+        _savedColor = 0;
+        _savedOpacity = 100;
+
+        layerRenderer.color = new Color
+        (
+            _contrastLayerColors[_savedColor].r,
+            _contrastLayerColors[_savedColor].g,
+            _contrastLayerColors[_savedColor].b,
+            _savedOpacity / 100
+        );
+
+        if (_contrastLayerEnabled.Value)
             layerRenderer.enabled = true;
         else
             layerRenderer.enabled = false;
-
-        UpdateOpacity();
     }
 
-    void UpdateOpacity()
+    public void ChangeLayerColor(Color newColor)
+    {
+        // Keep the colours to only be Black or White for now.
+        layerRenderer.color = newColor;
+
+        // If the colour passed was black, set the saved index to 0. Anything else will be 1/white.
+        _savedColor = newColor == Color.black ? 0 : 1;
+
+    }
+
+    public void ChangeLayerOpacity(float newOpacity)
     {
         Color currentColor = layerRenderer.color;
 
-        layerRenderer.color = new Color(currentColor.r, currentColor.g, currentColor.b, opacity.Value);
+        layerRenderer.color = new Color(currentColor.r, currentColor.g, currentColor.b, newOpacity);
+
+        _savedOpacity = Mathf.RoundToInt(newOpacity * 100);
     }
 }
