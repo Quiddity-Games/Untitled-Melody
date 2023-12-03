a25375a7b141b81dccf5cc557af028b673501b7f
seamusly <seamus.ly@outlook.com>
2023-12-03 02:42:38 -0500

    Amika animation controller and clips
    Remade due to an error in removing the previous controller from the prefab

--- Assets/PlayerAnimationController.cs
+++ Assets/PlayerAnimationController.cs
@@ -1,22 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerAnimationController : MonoBehaviour
 {
+    public static PlayerAnimationController Instance;
+
     private Animator _animator;
     private bool _facingRight;
+    public SpriteRenderer spriteRenderer;
 
-    private static readonly int IdleR = Animator.StringToHash("Idle (Right)");
-    private static readonly int IdleL = Animator.StringToHash("Idle (Left)");
-    private static readonly int DashMissR = Animator.StringToHash("Dash - Miss (Right)");
-    private static readonly int DashMissL = Animator.StringToHash("Dash - Miss (Left)");
-    private static readonly int DashGreatR = Animator.StringToHash("Dash - Great (Right)");
-    private static readonly int DashGreatL = Animator.StringToHash("Dash - Great (Left)");
-    private static readonly int DashPerfectR = Animator.StringToHash("Dash - Perfect (Right)");
-    private static readonly int DashPerfectL = Animator.StringToHash("Dash - Perfect (Left)");
-    private static readonly int BonkR = Animator.StringToHash("Bonk (Right)");
-    private static readonly int BonkL = Animator.StringToHash("Bonk (Left)");
+    private void Awake()
+    {
+        Instance = this;
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -27,55 +25,27 @@ public class PlayerAnimationController : MonoBehaviour
     public void SetDashRating(NoteTracker.HitInfo hitInfo, bool facingRight)
     {
         _facingRight = facingRight;
-        switch (hitInfo.rating)
-        {
-            case NoteTracker.BeatRating.MISS:
-                if (facingRight)
-                    _animator.CrossFade(DashMissR, 0, 0);
-                else
-                    _animator.CrossFade(DashMissL, 0, 0);
-                break;
-            case NoteTracker.BeatRating.GOOD:
-                if (facingRight)
-                    _animator.CrossFade(DashGreatR, 0, 0);
-                else
-                    _animator.CrossFade(DashGreatL, 0, 0);
-                break;
-            case NoteTracker.BeatRating.GREAT:
-                if (facingRight)
-                    _animator.CrossFade(DashGreatR, 0, 0);
-                else
-                    _animator.CrossFade(DashGreatL, 0, 0);
-                break;
-            case NoteTracker.BeatRating.PERFECT:
-                if (facingRight)
-                    _animator.CrossFade(DashPerfectR, 0, 0);
-                else
-                    _animator.CrossFade(DashPerfectL, 0, 0);
-                break;
-        }
-
-        StartCoroutine(BackToIdle());
+        _animator.SetInteger("Dash Rating", ((int)hitInfo.rating));
+        _animator.SetBool("Facing Right", facingRight);
+        _animator.SetTrigger("Dash");
     }
 
     public void PlayWallBump()
     {
-        if (_facingRight)
-            _animator.CrossFade(BonkR, 0, 0);
-        else
-            _animator.CrossFade(BonkL, 0, 0);
-
-        StartCoroutine(BackToIdle());
+        _animator.SetTrigger("Wall Bump");
+        _animator.SetBool("Facing Right", _facingRight);
     }
 
-    IEnumerator BackToIdle()
+    public void PlayRespawn(bool spawnRight)
     {
-        float animDuration = _animator.GetCurrentAnimatorClipInfo(0).Length;
+        _facingRight = spawnRight;
 
-        yield return new WaitForSeconds(animDuration);
-        if (_facingRight)
-            _animator.CrossFade(IdleR, 0, 0);
-        else
-            _animator.CrossFade(IdleL, 0, 0);
+        _animator.SetTrigger("Respawn");
+        _animator.SetBool("Facing Right", spawnRight);
+    }
+
+    void FinishRespawnClip()
+    {
+        RespawnManager.Instance.isRespawning = false;
     }
 }
