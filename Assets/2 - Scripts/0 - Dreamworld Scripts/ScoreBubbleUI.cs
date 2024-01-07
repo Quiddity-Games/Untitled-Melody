ae689bcd14c989a0aae0b7f726adbeefafcee3ad
seamusly <seamus.ly@outlook.com>
2024-01-07 03:40:04 -0500

    Score Bubble animation

--- Assets/2 - Scripts/0 - Dreamworld Scripts/ScoreBubbleUI.cs
+++ Assets/2 - Scripts/0 - Dreamworld Scripts/ScoreBubbleUI.cs
@@ -4,19 +4,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using DG.Tweening;
 
 public class ScoreBubbleUI : MonoBehaviour
 {
     [SerializeField] NoteTracker noteTracker;
 
-    [SerializeField] float offsetDuration;
-    [SerializeField] float verticalOffset;
     [SerializeField] TextMeshProUGUI ratingText;
     [SerializeField] TextMeshProUGUI timingText;
 
     private Animator scoreBubbleAnimator;
     private Image scoreBubbleImage;
-    private Vector2 originalPos;
     private GameObject timingObject;
 
     private void Awake()
@@ -29,22 +27,17 @@ public class ScoreBubbleUI : MonoBehaviour
     {
         scoreBubbleAnimator = GetComponent<Animator>();
         scoreBubbleImage = GetComponent<Image>();
-        originalPos = (transform as RectTransform).anchoredPosition;
+        //originalPos = (transform as RectTransform).anchoredPosition;
         timingObject = timingText.gameObject;
     }
 
 
     void ShowScoreBubble(NoteTracker.HitInfo hitInfo)
     {
-        (transform as RectTransform).anchoredPosition = originalPos; // Set bubble to original position.
+        //(transform as RectTransform).anchoredPosition = originalPos; // Set bubble to original position.
 
         // Reset the animation triggers on dash.
-        scoreBubbleAnimator.ResetTrigger("FadeIn");
-        scoreBubbleAnimator.ResetTrigger("FadeOut");
-
-        // Reset the animation if currently animating on dash.
-        if (scoreBubbleAnimator.GetBool("Fading"))
-            scoreBubbleAnimator.SetBool("Fading", false);
+        scoreBubbleAnimator.SetTrigger("PopIn");
 
         // Set timing text. If PERFECT, do not display text.
         switch (hitInfo.timing)
@@ -81,24 +74,5 @@ public class ScoreBubbleUI : MonoBehaviour
                 timingObject.SetActive(false);
                 break;
         }
-
-        StartCoroutine(FadeScoreBubble(offsetDuration, verticalOffset));
-    }
-
-    IEnumerator FadeScoreBubble(float duration, float offsetY)
-    {
-        Vector2 currentPos = (transform as RectTransform).anchoredPosition;
-        scoreBubbleAnimator.SetBool("Fading", true);
-        scoreBubbleAnimator.SetTrigger("FadeIn");
-        float time = 0f;
-
-        while (time < duration)
-        {
-            (transform as RectTransform).anchoredPosition = Vector2.Lerp(currentPos, new Vector2(currentPos.x, currentPos.y + offsetY), time / duration);
-            time += Time.deltaTime;
-            yield return null;
-        }
-
-        scoreBubbleAnimator.SetTrigger("FadeOut");
     }
 }
