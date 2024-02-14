8ae8651b5fbc10c63466ea2fce19fc3c9ffb2f22
Warspiteful <jalmendral24@gmail.com>
2024-02-13 21:29:21 -0500

    UM-121 and UM-12B: Integrate Wishing Well, On Min default, Toggle Switch fix

--- Assets/2 - Scripts/3 - UI Scripts/SwitchToggle.cs
+++ Assets/2 - Scripts/3 - UI Scripts/SwitchToggle.cs
@@ -42,15 +42,14 @@ public class SwitchToggle : MonoBehaviour {
    
    void OnSwitch (bool on) {
 
-      Debug.Log("ON SWITCH CALLED: " + (on ? "OFF" : "ON"));
       //uiHandleRectTransform.anchoredPosition = on ? handlePosition * -1 : handlePosition ; // no anim
-      uiHandleRectTransform.DOAnchorPos (on ? handlePosition * -1 : handlePosition, .4f).SetEase (Ease.InOutBack) ;
+      uiHandleRectTransform.DOAnchorPos (on ? handlePosition * -1 : handlePosition, .4f).SetEase (Ease.InOutBack).SetUpdate(true) ;
 
       //backgroundImage.color = on ? backgroundActiveColor : backgroundDefaultColor ; // no anim
-      backgroundImage.DOColor (on ? backgroundActiveColor : backgroundDefaultColor, .6f) ;
+      backgroundImage.DOColor (on ? backgroundActiveColor : backgroundDefaultColor, .6f).SetUpdate(true);
 
       //handleImage.color = on ? handleActiveColor : handleDefaultColor ; // no anim
-      handleImage.DOColor (on ? handleActiveColor : handleDefaultColor, .4f) ;
+      handleImage.DOColor (on ? handleActiveColor : handleDefaultColor, .4f).SetUpdate(true) ;
    }
 
    void OnDestroy ( ) {
