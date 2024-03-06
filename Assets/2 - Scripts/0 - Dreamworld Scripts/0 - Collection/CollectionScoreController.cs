52d67a84d0ef27142b7a7bffcdb38eb14b5e54c6
seamusly <seamus.ly@outlook.com>
2024-03-05 21:02:27 -0500

    Collectable event manager update

--- Assets/2 - Scripts/0 - Dreamworld Scripts/0 - Collection/CollectionScoreController.cs
+++ Assets/2 - Scripts/0 - Dreamworld Scripts/0 - Collection/CollectionScoreController.cs
@@ -10,7 +10,7 @@ public class CollectionScoreController : MonoBehaviour
 
     // Start is called before the first frame update
     
-    private int numCollectables = 0; //Total number of collectables in the level
+    public int numCollectables = 0; //Total number of collectables in the level
     [SerializeField] private int numCollected;    //Number of collectables the player has acquired so far
     [SerializeField] private int tempNumCollected; //"Temporary" information about collectables the player has claimed since their last checkpoint; used to determine what collectables they should lose / that should be reset the next time the player dies
     [SerializeField] private int requiredNumCollected;
@@ -18,6 +18,10 @@ public class CollectionScoreController : MonoBehaviour
     [SerializeField] private CollectableUI _ui;
     [SerializeField] private CollectionResetter resetter;
     [SerializeField] private EndScreenController endScreen;
+
+    public Action RegisterCollectable;
+
+    public Sprite[] CollectableSprites;
 
     private CollectableInfo endInfo;
     void Awake()
@@ -29,6 +33,8 @@ public class CollectionScoreController : MonoBehaviour
         numCollected = 0;
         tempNumCollected = 0;
         endInfo.ResetValues();
+
+        RegisterCollectable += () => numCollectables += 1;
     }
 
     public CollectableInfo GetCollectionStats()
@@ -38,12 +44,12 @@ public class CollectionScoreController : MonoBehaviour
     
     public void Start()
     {
-       
-        DreamworldEventManager.Instance.RegisterVoidEventResponse(DreamworldVoidEventEnum.DEATH, HandleDeath);
-        DreamworldEventManager.Instance.RegisterVoidEventResponse(DreamworldVoidEventEnum.REGISTER_COLLECTABLE, () => { 
+        /*DreamworldEventManager.Instance.RegisterVoidEventResponse(DreamworldVoidEventEnum.REGISTER_COLLECTABLE, () => {
             numCollectables += 1;
             UpdateInfo();
-        });
+        });*/
+
+        DreamworldEventManager.Instance.RegisterVoidEventResponse(DreamworldVoidEventEnum.DEATH, HandleDeath);
       
         DreamworldEventManager.Instance.RegisterVoidEventResponse(DreamworldVoidEventEnum.COLLECT, HandleCollection);
 
