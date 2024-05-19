afc84e1859d9083cf2698b5e450ef75ab63d13bd
Warspiteful <jalmendral24@gmail.com>
2024-05-18 22:15:04 -0400

    UM-23_73_FIX: Enable instantiate GameManager on start-up in build

--- Assets/2 - Scripts/2 - Utilities/ServicesLoader.cs
+++ Assets/2 - Scripts/2 - Utilities/ServicesLoader.cs
@@ -2,8 +2,6 @@ using UnityEngine;
 
 public static class ServicesLoader
 {
-#if UNITY_EDITOR
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
     public static void Execute() => Object.DontDestroyOnLoad(Object.Instantiate(Resources.Load("GameManager")));
-#endif
 }
