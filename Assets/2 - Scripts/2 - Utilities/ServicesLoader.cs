dad042458d09c111d515139a54447239225be1f1
Warspiteful <jalmendral24@gmail.com>
2024-06-02 11:17:37 -0400

    UM-23_73_FIX: Enable input on Android

--- Assets/2 - Scripts/2 - Utilities/ServicesLoader.cs
+++ Assets/2 - Scripts/2 - Utilities/ServicesLoader.cs
@@ -3,5 +3,8 @@ using UnityEngine;
 public static class ServicesLoader
 {
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
-    public static void Execute() => Object.DontDestroyOnLoad(Object.Instantiate(Resources.Load("GameManager")));
+    public static void Execute() {
+        Object.DontDestroyOnLoad(Object.Instantiate(Resources.Load("GameManager"))); 
+        Object.DontDestroyOnLoad(Object.Instantiate(Resources.Load("EventSystem"))); 
+     }
 }
