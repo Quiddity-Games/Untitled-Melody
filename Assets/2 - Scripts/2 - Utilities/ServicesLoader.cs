9d7ce0a64a2278119852c7dc00f751e6ce435b6c
Warspiteful <jalmendral24@gmail.com>
2024-06-15 09:02:06 -0400

    Revert "UM-23_73_FIX: Enable input on Android"
    This reverts commit dad042458d09c111d515139a54447239225be1f1.

--- Assets/2 - Scripts/2 - Utilities/ServicesLoader.cs
+++ Assets/2 - Scripts/2 - Utilities/ServicesLoader.cs
@@ -3,8 +3,5 @@ using UnityEngine;
 public static class ServicesLoader
 {
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
-    public static void Execute() {
-        Object.DontDestroyOnLoad(Object.Instantiate(Resources.Load("GameManager"))); 
-        Object.DontDestroyOnLoad(Object.Instantiate(Resources.Load("EventSystem"))); 
-     }
+    public static void Execute() => Object.DontDestroyOnLoad(Object.Instantiate(Resources.Load("GameManager")));
 }
