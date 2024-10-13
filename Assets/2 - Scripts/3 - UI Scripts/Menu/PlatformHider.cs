38ee5fdd533d114a63322d4b5eb8d92a0f707696
Justin Almendral <jalmendral24@gmail.com>
2024-10-13 00:37:50 -0400

    UM-162: Formatted Visual menu removing Windowed Mode

--- Assets/2 - Scripts/3 - UI Scripts/Menu/PlatformHider.cs
+++ Assets/2 - Scripts/3 - UI Scripts/Menu/PlatformHider.cs
@@ -8,21 +8,32 @@ public class PlatformHider : MonoBehaviour
     [SerializeField] List<GameObject> m_hideOnMobile;
     [SerializeField] List<GameObject> m_hideOnDesktop;
 
-    [SerializeField] private bool m_toggleDesktop;
-    [SerializeField] private bool m_toggleMobile;
+    [SerializeField] private bool m_toggleDesktop = false;
+    [SerializeField] private bool m_toggleMobile = false;
 
     public void Start()
     {
-        #if UNITY_STANDALONE || m_toggleDesktop
-        foreach(GameObject obj in m_hideOnDesktop)
-        {
-            obj.SetActive(false);
-        }
-        #elif UNITY_ANDROID || UNITY_IOS || m_toggleMobile
-        foreach(GameObject obj in m_hideOnMobile)
-        {
-            obj.SetActive(false);
-        }
+        #if UNITY_STANDALONE
+            m_toggleDesktop = true;
+            m_toggleMobile = false;
+        #elif UNITY_ANDROID || UNITY_IOS
+            m_toggleMobile = true;
+            m_toggleDesktop = false;
         #endif
+
+        if(m_toggleDesktop)
+        {
+            foreach(GameObject obj in m_hideOnMobile)
+            {
+                obj.SetActive(false);
+            }
+        }
+        if(m_toggleMobile)
+        {
+            foreach(GameObject obj in m_hideOnMobile)
+            {
+                obj.SetActive(false);
+            }
+        }
     }
 }
